6770ea088a869ff106a023dcc4fc375ac1ede0c7
Kevin Muñoz <harquin09@gmail.com>
2023-02-09 10:09:34 -0500

    side222

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//layout
-import AdminLayout from "./layouts/AdminLayout";
-import LayoutAuth from "./layouts/LayoutAuth";
-//Pages auth
-import Login from "./Pages/auth/Login";
-import Register from "./Pages/auth/Register";
-//Pages Admin
-import Home from "./Pages/admin/Home";
-import Error404 from "./Pages/Error404";
+// Layouts
+import LayoutAdmin from "./layouts/LayoutAdmin";
+// Pages auth
+import Login from "./pages/auth/Login";
+import ForgetPassword from "./pages/auth/ForgetPassword";
+// Pages admin
+import Home from "./pages/admin/Home";
+import Profile from "./pages/admin/Profile";
+import Chat from "./pages/admin/Chat";
+import Error404 from "./pages/Error404";
+import Tickets from "./pages/admin/Tickets";
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
         <Route path="/olvide-password" element={<ForgetPassword />} />
         <Route path="/" element={<LayoutAdmin />}>
           <Route index element={<Home />} />
+          <Route path="perfil" element={<Profile />} />
+          <Route path="chat" element={<Chat />} />
+          <Route path="tickets" element={<Tickets />} />
         </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
