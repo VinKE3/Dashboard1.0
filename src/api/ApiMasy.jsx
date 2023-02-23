a10d459efe0b0ac5a59571caac0b82d6f3c914b0
kevin munoz <harquin09@gmail.com>
2023-02-22 22:06:19 -0500

    funcionatoken

--- src/api/ApiMasy.jsx
+++ src/api/ApiMasy.jsx
@@ -1,5 +1,25 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default axios.create({
   baseURL: "https://mcwebapi.masydase.com/",
 });
+
+apiMasy.interceptors.request.use(
+  (config) => {
+    const token = authHelper.getAccessToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      navigate("/login");
+    }
+
+    config.headers["Content-Type"] = "application/json";
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
