ee054f788b214e66f592adb325e523a419214dce
Kevin Muñoz <harquin09@gmail.com>
2023-03-10 18:07:44 -0500

    filtro

--- src/Components/filtros/FiltroBasico.jsx
+++ src/Components/filtros/FiltroBasico.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-
+import * as Global from "../Global";
 const FiltroBasico = ({
   textLabel,
   inputPlaceHolder,
@@ -9,25 +9,21 @@ const FiltroBasico = ({
   inputMax,
   botonId,
   FiltradoButton,
-  /*filter,*/
   FiltradoKeyPress,
 }) => {
   return (
     <div className="flex mt-2 mb-2 rounded-1 border-gray-200 overflow-hidden md:text-sm">
-      <label className="inline-flex items-center px-3 text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 font-bold">
-        {textLabel}
-      </label>
+      <label className={Global.LabelStyle}>{textLabel}</label>
 
       <input
         type="text"
-        className="rounded-none bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full  border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-light dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className={Global.InputBotonStyle}
         autoComplete="off"
         spellCheck="false"
         placeholder={inputPlaceHolder}
         id={inputId}
         name={inputName}
         maxLength={inputMax}
-        // defaultValue={filter} //Filtro local
         onChange={FiltradoKeyPress}
       />
 
