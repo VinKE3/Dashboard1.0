74cf319a3fbe871788b2c0465c07bf1bcfb3348e
Kevin Muñoz <harquin09@gmail.com>
2023-04-11 16:45:48 -0500

    sidebarantigou

--- src/Components/SidebarComponent/Sidebar.jsx
+++ src/Components/SidebarComponent/Sidebar.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import BotonCerrarMenu from "../BotonesComponent/BotonCerrarMenu";
-import SidebarRoutes from "./SidebarRoutes";
+import SidebarMenus from "./SidebarMenus";
+import seccionesList from "./SeccionesMenus";
 
 const Sidebar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [showSubmenu, setShowSubmenu] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const handleActiveSection = (seccion) => {
+    setActiveSection(seccion);
+  };
+  const secciones = seccionesList();
 
   return (
     <>
-      <SidebarRoutes
+      <SidebarMenus
+        onclickButtonSubmenu={() => setShowSubmenu(!showSubmenu)}
+        showSubmenu={showSubmenu}
         showMenu={showMenu}
         onClickShowMenu={() => setShowMenu(!showMenu)}
+        secciones={secciones}
+        activeSection={activeSection}
+        handleActiveSection={handleActiveSection}
       />
       <BotonCerrarMenu
         onClickButton={() => setShowMenu(!showMenu)}
