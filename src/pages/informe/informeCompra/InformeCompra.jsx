a240287556241da39b59228a1f444672d1e76e7f
Lino <lino.masydase@gmail.com>
2023-05-26 17:51:01 -0500

    test234

--- src/pages/informe/informeCompra/InformeCompra.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const InformeCompra = () => {
-  return <div>Compras</div>;
-};
-
-export default InformeCompra;
--- src/pages/informe/informeVenta/InformeVenta.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const InformeVenta = () => {
-  return <div>Venta</div>;
-};
-
-export default InformeVenta;
