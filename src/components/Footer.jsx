dce0382c9c1d2108fbfd0c7d93ee45f78ca899ea
Lino <lino.masydase@gmail.com>
2023-06-08 12:49:20 -0500

    jueves08062023 footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
-
+import moment from "moment/moment";
 const Footer = () => {
   return (
     <footer className="h-full p-3 items-center ">
       <div className="flex items-center font-semibold">
-        <span>© 2023 - </span>
-        <span className="text-primary">MASY DATA SERVICE E.I.R.L.</span>
+        <p className="pr-1">{`© ${moment().format("YYYY")} - `}</p>
+        <p className="text-primary">{" MASY DATA SERVICE E.I.R.L."}</p>
       </div>
     </footer>
   );
